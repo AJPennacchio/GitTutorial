21ed5acbbc2b499328012fb02a2b0b8e7297c56e
AJPennacchio <apennacchio@oxy.edu>
2023-02-10 19:17:38 -0800

    Multiplication feature

--- Calc.py
+++ Calc.py
@@ -1,15 +1,18 @@
 import Add
+import Mult
 import Sub
 
 x = int(input("Enter a number: "))
 y = int(input("Enter another number: "))
 
-choice = input("Add(A), Subtract(S)")
+choice = input("Add(A), Subtract(S), Multiply(M)")
 
 if choice.lower() == 'a':
     Add.adder(x, y)
 elif choice.lower() == 's':
     Sub.subtr(x, y)
+elif choice.lower() == 'm':
+    Mult.multi(x, y)
 
 
 
