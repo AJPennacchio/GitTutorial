996908d850c24ed0efd2704c5907a80764f7b0a4
AJPennacchio <apennacchio@oxy.edu>
2023-02-10 19:11:49 -0800

    clean slate

--- Calc.py
+++ Calc.py
@@ -1,21 +1,15 @@
 import Add
-import Div
-import Mult
 import Sub
 
 x = int(input("Enter a number: "))
 y = int(input("Enter another number: "))
 
-choice = input("Add(A), Subtract(S), Multiplication(M)? ")
+choice = input("Add(A), Subtract(S)")
 
 if choice.lower() == 'a':
     Add.adder(x, y)
 elif choice.lower() == 's':
     Sub.subtr(x, y)
-elif choice.lower() == 'm':
-    Mult.multi(x, y)
-elif choice.lower() == 'd':
-    Div.divi(x, y)
 
 
 
--- Mult.py
+++
@@ -1,2 +0,0 @@
-def multi(x, y):
-    print("The product is", str(x * y))
